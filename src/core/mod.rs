4d420e9d8d5dbc7b2bc0de8f2f5c99c990f48c1b
Bilal el Issaoui <elissa_oui@outlook.com>
2026-08-26 10:07:27 +0200

    Add MyU64 and MyU256 to public exports

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,7 +1,7 @@
 pub mod diophantine;
 
 pub use diophantine::{
-    BranchFreeResult, DiophantinePair, MrsInt, ToBytes,
+    BranchFreeResult, DiophantinePair, MrsInt, ToBytes, MyU64, MyU256,
     calculate_anchor, calculate_popoviciu_cardinality,
     check_frobenius_bound, generate_representation_family,
     select_branch_free, select_branch_free_with_index,
