3899aa8ed2a56426abbd1af078152bfc2690095f
Bilal el Issaoui <elissa_oui@outlook.com>
2026-08-26 10:06:40 +0200

    Reorder merkle module exports in mod.rs

--- src/security/mod.rs
+++ src/security/mod.rs
@@ -5,4 +5,3 @@ pub mod merkle;
 pub use timecode::{generate_timecode, TimeCode};
 pub use lwe::{isolate_chain_parameter, verify_lwe_match, LweInstance, ToLweCoefficient};
 pub use merkle::{build_k_acceptance_root, verify_k_acceptance_proof, MerkleProof, hash_mrs_chain};
-
