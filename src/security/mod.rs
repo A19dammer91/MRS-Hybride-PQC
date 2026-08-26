a048ab33c78a49089d72e902d4d1cbc14435d178
Bilal el Issaoui <elissa_oui@outlook.com>
2026-08-26 07:57:06 +0200

    Remove code block formatting from mod.rs

--- src/security/mod.rs
+++ src/security/mod.rs
@@ -1,4 +1,3 @@
-```rust
 pub mod timecode;
 pub mod lwe;
 pub mod merkle;
@@ -6,4 +5,4 @@ pub mod merkle;
 pub use timecode::{generate_timecode, TimeCode};
 pub use lwe::{isolate_chain_parameter, verify_lwe_match, LweInstance, ToLweCoefficient};
 pub use merkle::{build_k_acceptance_root, verify_k_acceptance_proof, MerkleProof, hash_mrs_chain};
-```
+
