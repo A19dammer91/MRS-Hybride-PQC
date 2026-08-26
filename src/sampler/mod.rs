a2f2be88310364eb80ff908e958d7a87ff1cc827
Bilal el Issaoui <elissa_oui@outlook.com>
2026-08-26 07:29:32 +0200

    fix: remove ToBytes from sampler re-exports (not defined there)

--- src/sampler/mod.rs
+++ src/sampler/mod.rs
@@ -1,10 +1,8 @@
-```rust
 pub mod cdf_sampler;
 
 pub use cdf_sampler::{
-    MrsChain, SamplerInt, FromRandom, ToBytes,
+    MrsChain, SamplerInt, FromRandom,
     digital_root, validate_triangle_condition,
     count_triangle_filtered, check_ahead_valid,
     sample_three_layers, sample_three_layers_cdf, sample_triangle,
 };
-```
