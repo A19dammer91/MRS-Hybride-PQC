f40c60bfbc648f41e83659c407fdf3e2fd703aa2
Bilal el Issaoui <elissa_oui@outlook.com>
2026-08-27 10:01:44 +0200

    Expose additional functions and adversaries in mod.rs

--- src/security/mod.rs
+++ src/security/mod.rs
@@ -3,6 +3,9 @@ pub mod lwe;
 pub mod merkle;
 
 // Expose the submodules directly to the rest of the framework
-pub use timecode::{generate_timecode, TimeCode};
+pub use timecode::{
+    generate_timecode, TimeCode, run_euf_cma_game, run_forward_secrecy_game,
+    EufCmaAdversary, ForwardSecrecyAdversary
+};
 pub use lwe::{isolate_chain_parameter, verify_lwe_match, LweInstance};
 pub use merkle::{build_k_acceptance_root, verify_k_acceptance_proof, MerkleProof};
