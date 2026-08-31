fc5d9011fd592284b27d8d711fecf826dbc4e8d3
Bilal el Issaoui <elissa_oui@outlook.com>
2026-08-31 19:27:07 +0200

    Expose Shamir functions and structures

--- src/crypto/mod.rs
+++ src/crypto/mod.rs
@@ -1,6 +1,7 @@
 pub mod hybrid;
+pub mod shamir;
 
-// Expose the hybrid functions and structures
 pub use hybrid::{
     decrypt_payload_hybrid, derive_hybrid_key, encrypt_payload_hybrid, HybridCiphertextPacket,
 };
+pub use shamir::{split_secret, recover_secret, ShamirError};
