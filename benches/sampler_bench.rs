1f07c57cfc2bbdd986b08e0c6fd3c52a9f2b76a0
Bilal el Issaoui <elissa_oui@outlook.com>
2026-09-05 23:08:07 +0200

    Update benchmark to use sample_three_layers_safe

--- benches/sampler_bench.rs
+++ benches/sampler_bench.rs
@@ -1,9 +1,9 @@
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
-use mrs_auth_pqc::sampler::sample_three_layers;
+use mrs_auth_pqc::sampler::sample_three_layers_safe;
 use rand::rngs::OsRng;
 
 fn bench_sampler(c: &mut Criterion) {
-    let mut group = c.benchmark_group("sample_three_layers");
+    let mut group = c.benchmark_group("sample_three_layers_safe");
     let scales: [(&str, u64); 4] = [
         ("small_1e6", 1_000_003),
         ("moderate_1e9", 1_000_000_003),
@@ -13,7 +13,7 @@ fn bench_sampler(c: &mut Criterion) {
     for (label, n) in scales {
         group.bench_with_input(BenchmarkId::from_parameter(label), &n, |b, root| {
             let mut rng = OsRng;
-            b.iter(|| black_box(sample_three_layers(black_box(*root), &mut rng)));
+            b.iter(|| black_box(sample_three_layers_safe(black_box(*root), &mut rng)));
         });
     }
     group.finish();
