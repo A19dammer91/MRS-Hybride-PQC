41ba12ce250cea91f6e30aba9c8d45a78da4d966
Bilal el Issaoui <elissa_oui@outlook.com>
2026-09-08 09:44:12 +0200

    Add witness_supergrid module to security

--- src/security/mod.rs
+++ src/security/mod.rs
@@ -1,6 +1,7 @@
 pub mod merkle;
 pub mod timecode;
 pub mod witness;
+pub mod witness_supergrid;
 
 pub use timecode::{
     generate_timecode, run_euf_cma_game, run_forward_secrecy_game, EufCmaAdversary,
@@ -10,3 +11,5 @@ pub use timecode::{
 pub use merkle::{build_k_acceptance_root, verify_k_acceptance_proof, MerkleProof};
 
 pub use witness::{hash_chain, MasterSecret, Witness, WitnessSpace, WitnessStatus};
+
+pub use witness_supergrid::{Witness90, WitnessSpace90, WitnessStatus90, Alibi90};
